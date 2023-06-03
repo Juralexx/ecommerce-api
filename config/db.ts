e1751f6b67577861eabb6f14d04c95032115bb79
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-06-03 16:44:42 +0200

    updated db.ts

--- config/db.ts
+++ config/db.ts
@@ -1,16 +1,19 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config({ path: './.env' })
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 
-mongoose.set('strictQuery', false);
+//Assure qu'uniquement les valeur présents dans les models soit envoyé à la base de donnée
+mongoose.set('strictQuery', true);
 
-const database: string = process.env.MONGODB_URI.replace('<USER:PASSWORD>', `${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}`)
+//URL de connexion à la base de donnée
+const database: string = process.env.MONGODB_URI.replace('<USER:PASSWORD>', `${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}`);
 
+//Connexion à la base de donnée
 mongoose
     .connect(database)
     .then(() => {
-        console.log('Connexion à la base de donnée réussie !')
+        console.log('Base de donnée connectée.')
     })
     .catch(err => {
         console.log('Impossible de se connecter à la base de donnée : ', err)
-    })
+    });
