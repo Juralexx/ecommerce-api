f4f34ccfa3d09d820faf69d703c131ef58ba2815
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:40:31 +0200

    updated products.routes.ts

--- routes/products.routes.ts
+++ routes/products.routes.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response} from 'express'
-import ProductModel from '../models/product.model.ts'
-import { getProducts, updateProduct, createProduct } from '../controllers/products.controller.ts'
-import { deleteOne, getOne } from '../controllers/default.controllers.ts'
+import ProductModel from '../models/product.model.js'
+import { getProducts, updateProduct, createProduct, getProductByUrl } from '../controllers/products.controller.js'
+import { deleteOne, getOne } from '../controllers/default.controllers.js'
 
 const productsRoutes = express.Router()
 
 productsRoutes.get('/', getProducts)
+productsRoutes.get('/variant/:url', getProductByUrl)
 productsRoutes.get('/:id', (req: Request, res: Response) => getOne(req, res, ProductModel, { populate: 'images category promotions' }))
 productsRoutes.post('/create', createProduct)
 productsRoutes.put('/:id/update', updateProduct)
 productsRoutes.delete('/:id/delete', (req: Request, res: Response) => deleteOne(req, res, ProductModel))
 
-export default productsRoutes
+export default productsRoutes;
