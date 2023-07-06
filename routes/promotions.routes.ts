3bb52bc073c971b07a03090aba15b0d59c806e00
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:40:41 +0200

    updated promotions.routes.ts

--- routes/promotions.routes.ts
+++ routes/promotions.routes.ts
@@ -1,16 +1,22 @@
 import express, { Request, Response } from 'express'
-import { updatePromotion, createPromotion, deletePromotion } from '../controllers/promotions.controller.ts'
-import PromotionModel from '../models/promotion.model.ts'
-import { getAll, getOne } from '../controllers/default.controllers.ts'
+import { updatePromotion, createPromotion, deletePromotion } from '../controllers/promotions.controller.js'
+import PromotionModel from '../models/promotion.model.js'
+import { getAll, getOne } from '../controllers/default.controllers.js'
 
 const promotionsRoutes = express.Router()
 
 const population = [{
     path: 'condition.categories',
-    select: '_id name parent link'
+    select: '_id name parent link image',
+    populate: [
+        {
+            path: 'image',
+            select: '_id name path',
+        }
+    ]
 }, {
     path: 'condition.products',
-    select: '_id ref name category published price stock promotion images',
+    select: '_id ref name category published price variants stock promotion images',
     populate: [
         {
             path: 'images',
