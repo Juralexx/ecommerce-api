f6a94989c98a136bbf592cc08b67f07bbf2964bd
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:43:24 +0200

    updated user.default.model.ts

--- models/user.default.model.ts
+++ models/user.default.model.ts
@@ -1,64 +1,62 @@
 import mongoose from 'mongoose'
-import { DefaultUser, UserDocument } from '../types/types.ts';
+import { DefaultUser, UserDocument } from '../types/types.js';
 import { isEmailValid, isPasswordStrong, isValidName, isPhoneValid } from '../utils/utils.js';
 
-
-const DefaultUserModel = new mongoose.Schema<DefaultUser, UserDocument>(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-            validate: {
-                validator: (val: string) => isValidName(val),
-                message: 'Veuillez saisir un prénom valide.'
-            }
-        },
-        lastname: {
-            type: String,
-            required: true,
-            trim: true,
-            validate: {
-                validator: (val: string) => isValidName(val),
-                message: 'Veuillez saisir un nom valide.'
-            }
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            validate: {
-                validator: (val: string) => isEmailValid(val),
-                message: 'Veuillez saisir un email valide.'
-            }
-        },
-        password: {
-            type: String,
-            minlength: 8,
-            trim: true,
-            validate: {
-                validator: (val: string) => isPasswordStrong(val),
-                message: `Votre mot de passe ne respecte pas les conditions requises, celui-ci doit contenir au moins :
+const DefaultUserModel = new mongoose.Schema<DefaultUser, UserDocument>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+            validator: (val: string) => isValidName(val),
+            message: 'Veuillez saisir un prénom valide.'
+        }
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+            validator: (val: string) => isValidName(val),
+            message: 'Veuillez saisir un nom valide.'
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: (val: string) => isEmailValid(val),
+            message: 'Veuillez saisir un email valide.'
+        }
+    },
+    password: {
+        type: String,
+        minlength: 8,
+        trim: true,
+        validate: {
+            validator: (val: string) => isPasswordStrong(val),
+            message: `Votre mot de passe ne respecte pas les conditions requises, celui-ci doit contenir au moins :
                 ${'- Une majuscule'}
                 ${'- Une minuscule'}
                 ${'- Un chiffre'}
                 ${'- Un charactère spécial'}
                 ${'- Contenir 8 caractères'}`
-            }
-        },
-        phone: {
-            type: String,
-            validate: {
-                validator: (val: string) => isPhoneValid(val),
-                message: 'Veuillez saisir un numéro de téléphone valide'
-            }
         }
     },
-    {
-        timestamps: true,
-        minimize: false
+    phone: {
+        type: String,
+        validate: {
+            validator: (val: string) => {
+                if (val) isPhoneValid(val)
+            },
+            message: 'Veuillez saisir un numéro de téléphone valide'
+        }
     }
-);
+}, {
+    timestamps: true,
+    minimize: false
+});
 
-export default DefaultUserModel
+export default DefaultUserModel;
