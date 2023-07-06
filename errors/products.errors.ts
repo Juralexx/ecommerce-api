47cbcb38978c1c1328a17b323b2552d8540969d2
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:38:24 +0200

    updated product.errors.ts

--- errors/products.errors.ts
+++ errors/products.errors.ts
@@ -1,24 +1,20 @@
-export const productErrors = (err: any) => {
-    let errors: Record<string, any> = {}
+import { MongooseError } from "mongoose";
+
+export const productErrors = (err: MongooseError) => {
+    let errors: Record<string, string> = {};
 
     if (err.message.includes("ref"))
         errors.ref = "Veuillez saisir la référence du produit.";
-
     if (err.message.includes("name"))
         errors.name = "Veuillez saisir le nom du produit.";
-
     if (err.message.includes("content"))
         errors.content = "Veuillez saisir une description.";
-
     if (err.message.includes("category"))
-        errors.category = "Veuillez la catégorie.";
-
+        errors.category = "Veuillez sélectionner la catégorie.";
     if (err.message.includes("price"))
-        errors.price = "Veuillez saisir le prix.";
-
+        errors.price = "Veuillez saisir un prix valide.";
     if (err.message.includes("stock"))
         errors.stock = "Veuillez indiquer le nombre de produits en stock.";
-
     if (err.message.includes("images"))
         errors.images = "Veuillez ajouter des images.";
 
