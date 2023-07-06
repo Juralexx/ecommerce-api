23294ecc9a6d232255bcf74b3ddffde39ddfebcc
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:43:08 +0200

    updated product.model.ts

--- models/product.model.ts
+++ models/product.model.ts
@@ -1,108 +1,86 @@
 import mongoose, { Schema } from 'mongoose'
 
-const ProductModel: Schema = new mongoose.Schema(
-    {
-        published: {
-            type: Boolean,
-            required: true,
-            default: false,
-        },
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        category: {
+const ProductModel: Schema = new mongoose.Schema({
+    published: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'category',
+        required: true
+    },
+    images: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'media',
+        required: true
+    }],
+    variants: [{
+        _id: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'category',
-            required: true
+            default: new mongoose.Types.ObjectId()
         },
-        variants: [
-            {
-                _id: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    default: new mongoose.Types.ObjectId()
-                },
-                ref: {
-                    type: String
-                },
-                width: {
-                    type: String
-                },
-                height: {
-                    type: String
-                },
-                weight: {
-                    type: String
-                },
-                color: {
-                    type: String
-                },
-                price: {
-                    type: Number
-                },
-                stock: {
-                    type: Number
-                },
-                promotion: {
-                    type: Number
-                },
-                taxe: {
-                    type: Number
-                },
-                country: {
-                    type: Object,
-                    default: { name: "France", code: "FR" }
-                },
-                url: {
-                    type: String,
-                    required: true,
-                    trim: true
-                },
-                barcode: {
-                    type: String,
-                }
-            }
-        ],
-        promotions: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'promotion',
-            }
-        ],
-        images: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'media',
-                required: true
-            }
-        ],
-        description: {
+        ref: { type: String },
+        size: { type: String },
+        height: { type: String },
+        weight: { type: String },
+        color: { type: String },
+        price: {
+            type: Number,
+            min: 1
+        },
+        stock: { type: Number },
+        promotion: { type: Number },
+        taxe: { type: Number },
+        country: {
+            type: Object,
+            default: { name: "France", code: "FR" }
+        },
+        url: {
             type: String,
-            required: false,
+            required: true,
             trim: true
         },
-        content: {
-            type: String,
-            required: false,
-            trim: true
-        },
-        details: {
-            type: [],
-            detail: {
-                name: String,
-                value: String
+        barcode: { type: String }
+    }],
+    base_variant: {
+        type: Object,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    details: {
+        type: [],
+        detail: {
+            name: {
+                type: String
+            },
+            value: {
+                type: String
             }
-        },
-        tags: {
-            type: [String]
         }
     },
-    {
-        timestamps: true,
-        minimize: false
-    }
-);
+    promotions: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'promotion',
+    }]
+}, {
+    timestamps: true,
+    minimize: false
+});
 
-
-export default mongoose.model("product", ProductModel)
+export default mongoose.model("product", ProductModel);
