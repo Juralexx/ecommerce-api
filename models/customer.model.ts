3b0d7ed2e437ee45126a9e4f123501e250c67733
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:42:47 +0200

    updated customer.model.ts

--- models/customer.model.ts
+++ models/customer.model.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
-import * as argon2 from 'argon2'
-import DefaultUserModel from './user.default.model.ts';
+import DefaultUserModel from './user.default.model.js';
 import { UserDocument, ICustomer } from '../types/types';
-import { login } from './plugins.ts'
+import { login } from './plugins.js'
 import { isValidPostcode } from '../utils/utils.js';
+import bcrypt from 'bcryptjs'
 
 const CustomerModel = new mongoose.Schema<ICustomer, UserDocument>(
     {
@@ -11,7 +11,8 @@ const CustomerModel = new mongoose.Schema<ICustomer, UserDocument>(
         title: {
             type: String,
             enum: ['M', 'Mme'],
-            required: true
+            required: true,
+            default: 'M'
         },
         birth: {
             type: Date,
@@ -19,12 +20,28 @@ const CustomerModel = new mongoose.Schema<ICustomer, UserDocument>(
         addresses: {
             type: [],
             address: {
+                name: {
+                    type: String,
+                    required: true
+                },
+                lastname: {
+                    type: String,
+                    required: true
+                },
+                society: {
+                    type: String,
+                },
                 street: {
                     type: String,
+                    required: true,
                     minLength: 5
+                },
+                complement: {
+                    type: String
                 },
                 postcode: {
                     type: String,
+                    required: true,
                     validate: {
                         validator: (val: string) => isValidPostcode(val),
                         message: 'Veuillez saisir un code postal valide.'
@@ -32,15 +49,18 @@ const CustomerModel = new mongoose.Schema<ICustomer, UserDocument>(
                 },
                 city: {
                     type: String,
+                    required: true,
                     minLength: 2
                 },
                 department: {
-                    type: String,
-                    minLength: 3
+                    type: String
                 },
                 region: {
+                    type: String
+                },
+                phone: {
                     type: String,
-                    minLength: 3
+                    required: true
                 }
             }
         },
@@ -64,8 +84,10 @@ const CustomerModel = new mongoose.Schema<ICustomer, UserDocument>(
     }
 );
 
+//Avant le sauvegarde du document dans la BDD nous hashons le mot de passe
 CustomerModel.pre("save", async function (next) {
-    this.password = await argon2.hash(this.password);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
     next();
 })
 
