551baf97964f2800ac856af236909cdd7e2f8af2
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:43:41 +0200

    updated plugins.ts

--- models/plugins.ts
+++ models/plugins.ts
@@ -1,19 +1,33 @@
-import * as argon2 from 'argon2'
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
+
+/**
+ * Login plugin function
+ * @param schema Mongoose user schema
+ */
 
 export function login(schema: any) {
     schema.statics.login = async function (email: string, password: string) {
+        //Verification that the email is attached to a user
         const user = await this.findOne({ email });
+        //Si l'utilisateur existe
         if (user) {
-            const isSamePassword = await argon2.verify(user.password, password);
+            //Password hash decoding
+        const isSamePassword = await bcrypt.compare(password, user.password);
+            //Si le mot de passe est le même nous retournons l'utilisateur
             if (isSamePassword) {
                 return user;
-            } else {
+            }
+            //Otherwise we return a password error
+            else {
                 throw {
                     name: 'password',
                     message: 'Mot de passe incorrect.'
                 };
             }
-        } else {
+        }
+        //Otherwise we return an error 'cause the email does not exist in the database
+        else {
             throw {
                 name: 'email',
                 message: 'Cet email n\'est rattachée à aucun compte.'
@@ -21,3 +35,4 @@ export function login(schema: any) {
         }
     };
 }
+
