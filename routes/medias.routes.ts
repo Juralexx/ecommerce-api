3342c5d11685df85f5a23ebaca37496282f263e1
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-07-06 14:40:03 +0200

    updated medias.routes.ts

--- routes/medias.routes.ts
+++ routes/medias.routes.ts
@@ -1,17 +1,17 @@
 import express, { Request, Response } from 'express'
-import FilesModel from '../models/files.model.ts';
-import { updateImage, upload, uploadImage, uploadImages } from '../controllers/upload.controller.ts'
-import { deleteMedia } from '../controllers/medias.controller.ts'
-import { getAll, getOne } from '../controllers/default.controllers.ts'
+import FilesModel from '../models/files.model.js';
+import { updateImage, upload, uploadImage, uploadImages } from '../controllers/upload.controller.js'
+import { deleteMedia } from '../controllers/medias.controller.js'
+import { getAll, getOne } from '../controllers/default.controllers.js'
 
 const mediasRoutes = express.Router();
-
-mediasRoutes.get('/', (req: Request, res: Response) => getAll(req, res, FilesModel));
-mediasRoutes.get('/:id', (req: Request, res: Response) => getOne(req, res, FilesModel));
-mediasRoutes.delete('/:id/delete', deleteMedia)
 
 mediasRoutes.post('/upload/single', upload.single('file'), uploadImage);
 mediasRoutes.post('/upload/multiple', upload.array('files', 5), uploadImages);
 mediasRoutes.put('/:id/update', upload.single('file'), updateImage);
+
+mediasRoutes.get('/', (req: Request, res: Response) => getAll(req, res, FilesModel));
+mediasRoutes.get('/:id', (req: Request, res: Response) => getOne(req, res, FilesModel));
+mediasRoutes.delete('/:id/delete', deleteMedia);
 
 export default mediasRoutes;
